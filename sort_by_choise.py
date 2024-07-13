56c3f97764fd64e575614624a1f60f274131c77d
whitek <whitenet@dev.com>
2024-07-13 14:29:30 +0300

    fix binary

---
+++ sort_by_choise.py
@@ -0,0 +1,11 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+
+    return smallest_index
+
+def selectionSort(arr):
